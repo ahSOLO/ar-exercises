2a8b4feb0178935a04dd3e611e1a694babbdf9c0
Andrew Han <junmenghan@gmail.com>
2021-03-02 01:33:34 +0000

    complete exercises

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,16 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Kay", last_name: "Vee", hourly_rate: 41)
+@store1.employees.create(first_name: "Knush", last_name: "Popo", hourly_rate: 100)
+@store2.employees.create(first_name: "Andy", last_name: "Roo", hourly_rate: 120)
+@store2.employees.create(first_name: "Pop", last_name: "Tart", hourly_rate: 80)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,22 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  validates :name, presence: true, length: {minimum: 3}
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :carries_at_least_one_sex
+
+  def carries_at_least_one_sex
+    errors.add(:mens_apparel, "store must carry at least one sex") if (mens_apparel.nil? && womens_apparel.nil?)
+    errors.add(:womens_apparel, "store must carry at least one sex") if (mens_apparel.nil? && womens_apparel.nil?)
+  end
+end
+
+class Employee < ActiveRecord::Base
+  validates :first_name, :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than: 40, less_than: 200}
+  has_one :store
+end
+
+store_name = gets.chomp
+Store.create(name: store_name).errors.each { |attrib, message| puts "#{attrib}, #{message}" }
